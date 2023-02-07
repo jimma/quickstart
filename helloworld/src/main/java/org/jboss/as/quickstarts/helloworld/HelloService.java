e73e0c996a5823a8e429437736c2eb3f4577d2c1
Jim Ma <jimma@JimMacPro.local>
2023-02-07 20:49:07 +0800

    Add more

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello hello " + name + "!";
+        return "Hello hello  3333 " + name + "!";
     }
 
 }
